489ec51f3ad20ca25af8d5a7452ef9210c1e025e
6six7sven <sushant.679p@gmail.com>
2022-07-15 18:42:20 +0530

    show results and smol fetch factor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
 import MainPageLayout from "../components/MainPageLayout";
+import { apiGet } from "../misc/config";
+
+
 
 const Home = () => {
     const [input, setInput] = useState('');
-
+    const [results, setResults] = useState(null);
     const onInputChange = (ev) => {
         setInput(ev.target.value);
 
     }
+
+    const onSearch = () => {
+    
+        apiGet(`/search/shows?q=${input}`).then(result => {
+            setResults(result);
+            console.log(result);
+        })
+
+    };
 
     const onKeyDown = (ev) => {
         if (ev.keyCode === 13)
@@ -17,20 +29,37 @@ const Home = () => {
         console.log(ev.keyCode);
     }
 
-    const onSearch = () => {
-    
+    const renderResults = () => {
 
-        // browser api fetches https://api.tvmaze.com/search/shows?q=girls
-        fetch (`https://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json()).then(result =>{
-            console.log(result);
-        }) //interpolated string to pass input to the web api
-    };
+
+        if(results && results.length === 0)
+        {
+            return <div>
+                no results
+            </div>
+        }
+
+        if(results && results.length > 0)
+        {
+            return <div>
+                
+                {results.map(item => (
+                    <div key = {item.show.id}>{item.show.name} </div>
+                ))}
+            </div>
+        }
+
+        return null;
+    }
+
+
 
     return (
         <MainPageLayout> 
             <input type = "text" onChange = {onInputChange} onKeyDown={onKeyDown} value={input}/>
             <button type = "button" onClick = {onSearch}> Search </button>
 
+            {renderResults()}
       
         </MainPageLayout>
     );
