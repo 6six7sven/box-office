6fb4e651702bd3d3823eefc1d6550fb4b72698fb
6six7sven <sushant.679p@gmail.com>
2022-07-19 15:42:55 +0530

    added show page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Show from './pages/Show';
 
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 
 
 function App() {
-  return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
+	return (
+		<Switch>
+			<Route exact path="/">
+				<Home />
+			</Route>
 
-      <Route exact path="/starred">
-        <Starred />
-      </Route>
+			<Route exact path="/starred">
+				<Starred />
+			</Route>
 
-      <Route>
-        <div>
-          Not found
-        </div>
+			<Route exact path="/show/:id">
 
-      </Route>
-    </Switch>
-  );
+			<Show />
+
+
+			</Route>
+
+			<Route>
+				<div>
+					Not found
+				</div>
+
+			</Route>
+		</Switch>
+	);
 }
 
 export default App;
