52a4ab19248d77d01948ec7b551d6c9ab2452c98
6six7sven <sushant.679p@gmail.com>
2022-07-19 17:46:29 +0530

    created show page components and added components

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useReducer } from "react";
 import { useParams } from "react-router-dom";
+import Cast from "../components/show/Cast";
+import Details from "../components/show/Details";
+import Seasons from "../components/show/Seasons";
+import ShowMainData from "../components/show/ShowMainData";
 import { apiGet } from "../misc/config";
 
 const reducer = (prevState, action) => {
@@ -58,7 +62,7 @@ const Show = () => {
 
     if(isLoading) {
         return <div>
-            Date is being loaded
+            Data is being loaded
         </div>
     }
 
@@ -68,13 +72,36 @@ const Show = () => {
         </div>
     } 
 
-    return (
-        <div>
-            this is show page
+    return (<div>
+            <ShowMainData 
+                image = {show.image} 
+                name = {show.name} 
+                rating = {show.rating} 
+                summary = {show.summary} 
+                tags = {show.genres} 
+                />
+            <div>
+                <h2>
+                    Details
+                </h2>
+                <Details status={show.status} network={show.network} premiered = {show.premiered}/>
+            </div>
+            <div>
+            <h2>
+                    Seasons
+                </h2>
+                <Seasons seasons = {show._embedded.seasons}/>
+            </div>
+            <div>
+            <h2>
+                    Cast
+                </h2>
+                <Cast cast = {show._embedded.cast}/>
+            </div>
+
         </div>
     )
-
-}
+};
 
 
 export default Show;
