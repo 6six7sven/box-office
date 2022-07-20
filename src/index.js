ec683bb2a7256071457967191ab6bde8646adec5
6six7sven <sushant.679p@gmail.com>
2022-07-20 22:48:53 +0530

    deployed app and switched to hashrouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {    BrowserRouter    } from 'react-router-dom';
+import {    HashRouter    } from 'react-router-dom'; // this one makes the website work for client side routing only
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
@@ -8,9 +8,9 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
   );
 
