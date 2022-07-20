63f30c8e177eeac4d8150b347cb6fff17aec0307
6six7sven <sushant.679p@gmail.com>
2022-07-20 19:16:39 +0530

    custom hooks for starred shows

--- src/components/show/ShowCard.js
+++ src/components/show/ShowCard.js
@@ -1,30 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Star } from "../styled";
 import { StyledShowCard } from "./ShowCard.styled";
 
 
 // import { SearchCard } from "../styled";
 
-const ShowCard = ({ id, image, name, summary }) => {
-    const summaryAsText = summary
-      ? `${summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, "")}...`
-      : 'No description';
-  
-    return (
-      <StyledShowCard>
-        <div className = " img-wrapper ">
-          <img src={image} alt="show" />
-        </div>
-  
-        <h1>{name}</h1>
-  
-        <p>{summaryAsText}</p>
-  
-        <div className="btns">
-          <Link to={`/show/${id}`}>Read more</Link>
-          <button type="button">Star me</button>
-        </div>
-      </StyledShowCard>
-    );
-  };
+const ShowCard = ({ id, image, name, summary, onStarClick, isStarred }) => {
+	const summaryAsText = summary
+		? `${summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, "")}...`
+		: 'No description';
+
+	return (
+		<StyledShowCard>
+			<div className=" img-wrapper ">
+				<img src={image} alt="show" />
+			</div>
+
+			<h1>{name}</h1>
+
+			<p>{summaryAsText}</p>
+
+			<div className="btns">
+				<Link to={`/show/${id}`}>Read more</Link>
+				<button type="button" onClick={onStarClick}>
+					<Star active={isStarred}/>
+				</button>
+			</div>
+		</StyledShowCard>
+	);
+};
 export default ShowCard;
